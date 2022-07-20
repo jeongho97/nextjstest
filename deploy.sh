a1b51641ad53d80b2ed90c9d8352207c9b37ae8d
이정호 <skjongho@naver.com>
2022-07-20 11:50:06 +0900

    test5

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/front
+sudo rm -rf node_modules
 sudo npm install
 sudo npm run start
--- stop_server.sh
+++ stop_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo fuser -k 3000/tcp
-cd /home/ubuntu/front
-sudo rm -rf node_modules
+
