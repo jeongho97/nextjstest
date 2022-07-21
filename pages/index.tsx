950247f63e8285e1b01479f60545c0f26624c6fc
이정호 <skjongho@naver.com>
2022-07-21 09:52:19 +0900

    final test nextjs jenkins nginx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to jenkins nginx <a href="https://nextjs.org">Next.js!</a>
+          Welcome to aws jenkins nginx <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
